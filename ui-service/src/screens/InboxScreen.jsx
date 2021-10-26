8d74142310f5a96b9f4dc30d225ffa250ec11c22
snhumphr <snhumphr@ualberta.ca>
2021-10-25 20:48:42 -0600

    fixed detached head, ugh

--- ui-service/src/screens/InboxScreen.jsx
+++ ui-service/src/screens/InboxScreen.jsx
@@ -2,6 +2,50 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 export const InboxScreen = (props) => {
+
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        fetchPosts();
+    }, [])
+
+    const fetchPosts = async () => {
+        let testjson = [
+            {
+                "type": "friendRequest",
+                "sender": "October Snake",
+                "recipent": "Madeline Noodle",
+                "senderHost": "inserturlhere",
+                "date": "2021-10-09T13:07:04+00:00"
+            },
+
+            {
+                "type": "like",
+                "sender": "October Snake",
+                "recipent": "Madeline Noodle",
+                "senderHost": "inserturlhere",
+                "postID": "insertpostIDhere",
+                "date": "2021-10-09T13:07:04+00:00"
+            },
+
+            {
+                "type": "comment",
+                "sender": "Jill Yukon",
+                "recipient": "Madeleine Noodle",
+                "senderHost": "inserturlhere",
+                "date": "2021-10-09T13:07:04+00:00",
+                "contentType": "text/plain",
+                "content": "Nice cat! But it could be better...."
+            }
+
+            
+        ]
+
+
+
+        setPosts(testjson)
+    }
+
     return (
         <>
             <div>
