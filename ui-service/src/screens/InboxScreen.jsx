3eeb640633c3ebe92a207396ff9f1fe166189bc8
snhumphr <snhumphr@ualberta.ca>
2021-10-27 19:28:30 -0600

    Broke up the megafunction into smaller chunks so it actually works

--- ui-service/src/screens/InboxScreen.jsx
+++ ui-service/src/screens/InboxScreen.jsx
@@ -51,36 +51,46 @@ export const InboxScreen = (props) => {
     const parsePosts = function(postsArray){
 
         var displayArray = []
-        var buttonArray= []
 
         for(var i = 0;i < postsArray.length; i++){
             var postType = postsArray[i].type
             if (postType === "friendRequest") {
+                displayArray[i] = friendRequest(postsArray[i], i)
                 //TODO: ADD BUTTONS TO ACCEPT/DENY THE FRIEND REQUEST HERE
-                //TODO: HAVE THE BUTTONS THAT ACCEPT/DENY DELETE THE REQUEST
-                displayArray[i] = " would like to be your friend!"
-                buttonArray[i] = ["","Accept Friend Request"]
             } else if (postType === "like") {
-                //TODO: ADD  link to the post that was liked
-                displayArray[i] = " liked your post!"
-                buttonArray[i] = ["","See liked post"]
+                displayArray[i] = likedPost(postsArray[i], i)
             } else if (postType === "comment") {
-                //TODO: ADD  link to the post that was COMMENTED ON
-                displayArray[i] = " commented '" + postsArray[i].content + "'!"
-                buttonArray[i] = ["","See commented post"]
+                displayArray[i] = commentedPost(postsArray[i], i)
             } else{
                 //TODO: Throw a proper error here
             }
         }
 
-        return displayArray.map((post, i) => 
-            
-            <div className="shadow w-75 mb-5 mt-3 mx-auto border p-5 rounded rounded-5 z-depth-2 bg-white" key={"post"+i}>
-                <a href={postsArray[i].senderHost + "/author/" + postsArray[i].senderID} >{postsArray[i].senderName}</a> {post}
-                <br></br><button className = "btn btn-primary"> {buttonArray[i][1]} </button> <button className="btn btn-danger"> Dismiss> </button>
-            </div>
-            
-        )
+        return displayArray
+    }
+
+    const friendRequest = function(request, i){
+
+        return  <div className="shadow w-75 mb-5 mt-3 mx-auto border p-5 rounded rounded-5 z-depth-2 bg-white" key={"post"+i}>
+                    <a href={request.senderHost + "/author/" + request.senderID} >{request.senderName}</a> wants to be friends!
+                    <br></br><button className = "btn btn-primary"> Accept friend request </button> <button className="btn btn-danger"> Dismiss </button>
+                </div>
+    }
+
+    const likedPost = function(like, i){
+
+        return  <div className="shadow w-75 mb-5 mt-3 mx-auto border p-5 rounded rounded-5 z-depth-2 bg-white" key={"post"+i}>
+                    <a href={like.senderHost + "/author/" + like.senderID} >{like.senderName}</a> liked your post!
+                    <br></br><button className = "btn btn-primary"> Goto liked post </button> <button className="btn btn-danger"> Dismiss </button>
+                </div>
+    }
+
+    const commentedPost = function(comment, i){
+
+        return  <div className="shadow w-75 mb-5 mt-3 mx-auto border p-5 rounded rounded-5 z-depth-2 bg-white" key={"post"+i}>
+                    <a href={comment.senderHost + "/author/" + comment.senderID} >{comment.senderName}</a> commented '{comment.content}' on your post!
+                     <br></br><button className = "btn btn-primary"> Goto commented post </button> <button className="btn btn-danger"> Dismiss </button>
+                </div>
     }
 
     return (
