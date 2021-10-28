f96a08f23dfaa3e9da988d7a3a1f4ad2c928dd88
janhavi0210 <75977053+janhavi0210@users.noreply.github.com>
2021-10-28 15:49:35 -0600

    Update FriendScreen.jsx

--- ui-service/src/screens/FriendScreen.jsx
+++ ui-service/src/screens/FriendScreen.jsx
@@ -1,14 +1,88 @@
 import React from 'react'
 import { Link } from "react-router-dom";
+import {Header} from '../components/Header'
+import {List} from '../components/List'
+import {useState, useEffect} from 'react'
+
 export const FriendScreen = (props) => {
-    return (
-        <>
-            <div>
-                This is a Friend Screen!
-            </div>
-            <Link to="/">
-                <button className="btn btn-danger" href="/">Go Back to Main Menu</button>
-            </Link>
-        </>
+    const [Friends, setFriends]= useState([
+      {
+        "type":"author",
+        "id":"http://127.0.0.1:5454/author/1d698d25ff008f7538453c120f581471",
+        "url":"http://127.0.0.1:5454/author/1d698d25ff008f7538453c120f581471",
+        "host":"http://127.0.0.1:5454/",
+        "displayName":"Greg Johnson",
+        "github": "http://github.com/gjohnson",
+        "profileImage": "https://i.imgur.com/k7XVwpB.jpeg"
+    },
+    {
+      "type":"author",
+      "id":"http://127.0.0.1:5454/author/1d698d25ff008f7538453c120f581471",
+      "url":"http://127.0.0.1:5454/author/1d698d25ff008f7538453c120f581471",
+      "host":"http://127.0.0.1:5454/",
+      "displayName":"Treg Johnson",
+      "github": "http://github.com/gjohnson",
+      "profileImage": "https://i.imgur.com/k7XVwpB.jpeg"
+  },
+  {
+    "type":"author",
+    "id":"http://127.0.0.1:5454/author/1d698d25ff008f7538453c120f581471",
+    "url":"http://127.0.0.1:5454/author/1d698d25ff008f7538453c120f581471",
+    "host":"http://127.0.0.1:5454/",
+    "displayName":"Kreg Johnson",
+    "github": "http://github.com/gjohnson",
+    "profileImage": "https://i.imgur.com/k7XVwpB.jpeg"
+  },
+  {
+    "type":"author",
+    "id":"http://127.0.0.1:5454/author/1d698d25ff008f7538453c120f581471",
+    "url":"http://127.0.0.1:5454/author/1d698d25ff008f7538453c120f581471",
+    "host":"http://127.0.0.1:5454/",
+    "displayName":"Greg Johnson",
+    "github": "http://github.com/gjohnson",
+    "profileImage": "https://i.imgur.com/k7XVwpB.jpeg"
+  },
+  {
+    "type":"author",
+    "id":"http://127.0.0.1:5454/author/1d698d25ff008f7538453c120f581471",
+    "url":"http://127.0.0.1:5454/author/1d698d25ff008f7538453c120f581471",
+    "host":"http://127.0.0.1:5454/",
+    "displayName":"Greg Johnson",
+    "github": "http://github.com/gjohnson",
+    "profileImage": "https://i.imgur.com/k7XVwpB.jpeg"
+  }]) // Temporary data
+
+  /*
+  //Fetch friends from json server
+  useEffect( () => {
+    const getFriends= async () => {
+      const friendsFromServer= await fetchFriends()
+      setFriends(friendsFromServer)
+    }
+    
+    getFriends()
+  }, [])
+
+  const fetchFriends= async () =>
+    {
+      const result = await fetch('http://localhost:5000/Friends') //Temporary Server
+      const data = await result.json()
+      return data
+    }*/
+
+    
+
+  return (
+    <div className="container">
+      <div>
+      <Header title= "Friends" />
+      
+      </div>
+      <List friends={Friends} />
+
+      
+    </div>
     )
-}
+    }
+
+
