981339b1a27efc80c841b050e732760b6821ae07
wonbinjeong <jeongwonbin0503@gmail.com>
2021-10-28 16:30:49 -0600

    deploying

--- rest-service/server.js
+++ rest-service/server.js
@@ -8,7 +8,7 @@ app.use(cors())
 const authorRoutes = require('./routes/author')
 app.use("/service/author", authorRoutes);
 
-app.use('/', (req, res, next) =>{
+app.use('/', (req, res, next) => {
     res.status(404).send('404 Page Not Found');
 });
 
