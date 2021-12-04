0993ccf6cc544968eb7077823a1381e5132ad7b4
snhumphr <snhumphr@ualberta.ca>
2021-12-03 19:41:45 -0700

    connet->connect

--- ui-service/src/screens/MainScreen.jsx
+++ ui-service/src/screens/MainScreen.jsx
@@ -89,7 +89,7 @@ export const MainScreen = () => {
                 <div className="col">
                     <Button className="Buttons" onClick={() => {navigate("/Posts/Others")}}>Go to Post Others</Button>
                     <Button className="Buttons" onClick={() => {navigate("/Inbox")}}>Go to Inbox</Button>
-                    <Button className="Buttons" onClick={() => {navigate("/Author/Foreign")}}>Connet to Foreign Authors</Button>
+                    <Button className="Buttons" onClick={() => {navigate("/Author/Foreign")}}>Connect to Foreign Authors</Button>
                 </div>
             </div>
             {/* </Link>   */}
