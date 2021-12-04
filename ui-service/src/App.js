35722b895c436a8ba601a6b22ace8efa0fe0d293
snhumphr <snhumphr@ualberta.ca>
2021-12-03 18:52:42 -0700

    Made sure that the author homepage is the actual main screen instead of a black page

--- ui-service/src/App.js
+++ ui-service/src/App.js
@@ -61,7 +61,7 @@ function App() {
               <Route
                 path="/Home"
                 name="Home Screen"
-                element={<BlankPage /> }/>
+                element={<MainScreen /> }/>
               </>
             ) : <></>}
             {/* <Route
