3e618e764ab1aaa6f0bf4536ab0a60566635b5e0
Student <atripodi@ualberta.ca>
2021-12-06 16:03:33 -0500

    more consistent UI

--- ui-service/src/screens/AdminAuthorScreen.jsx
+++ ui-service/src/screens/AdminAuthorScreen.jsx
@@ -41,23 +41,29 @@ export const AdminAuthorScreen = (props) => {
     }
 
     return (
-        <div className="container">
-            <div  align="center">
-                <Button className="Buttons mx-2" onClick={() => {navigate("/Admin")}}>Return to main</Button>
-                <Button className="Buttons mx-2" onClick={() => {logOutHandler()}}>Logout</Button>
-            </div>
-            <div className="row">
-                {authors.map((author, i) =>
+        <body className="background">
+            <div id="colortheme" className="container" style={{display: 'flex', flexDirection: 'column', justifyContent: 'space-around'}}>
+                <div  align="center">
+                    <Button className="Buttons mx-2" onClick={() => {navigate("/Admin")}}>Return to main</Button>
+                    <Button className="Buttons mx-2" onClick={() => {logOutHandler()}}>Logout</Button>
+                </div>
+                <div style={{display: 'flex', gap: "20px"}} className="row">
+                    {authors.map((author, i) =>
                     <div>
                         <img className= 'profileImage' src= {author["profileImage"]} alt=""></img>
                         {author.displayName}
-                        <Button className="Buttons mx-2" onClick={() => {navigate(`/Admin/Authors/Profile?authorID=${author.id}`)}}>Edit Profile</Button>
-                        <Button className="Buttons mx-2" onClick={() => {navigate(`/Admin/Authors/Friends?authorID=${author.id}`)}}>View Friends</Button>
-                        <Button className="Buttons mx-2" onClick={() => {deleteAuthorHandler(author)}}>Delete Author</Button>
+                        <div style={{display: 'flex', justifyContent: 'flex-end'}}>
+                            <Button className="Buttons mx-2" onClick={() => {navigate(`/Admin/Authors/Profile?authorID=${author.id}`)}}>Edit Profile</Button>
+                            <Button className="Buttons mx-2" onClick={() => {navigate(`/Admin/Authors/Friends?authorID=${author.id}`)}}>View Friends</Button>
+                            <Button className="Buttons mx-2" onClick={() => {deleteAuthorHandler(author)}}>Delete Author</Button>
+                        </div>
+                        
                     </div>
-                )}
-            </div>
-            <button className= "Buttons mx-2" onClick={() => navigate("/Admin/Authors/Create")}> Add Author </button>
-        </div>
+                    )}
+                </div>
+                <Button className= "Buttons mx-2" onClick={() => navigate("/Admin/Authors/Create")}> Add Author </Button>
+                </div>
+        </body>
+        
     )
 }
