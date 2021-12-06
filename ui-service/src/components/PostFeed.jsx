c1ed36e54110bc07cb044299cdcb6780600f410c
snhumphr <snhumphr@ualberta.ca>
2021-12-05 21:31:36 -0700

    Added support for markdown comments

--- ui-service/src/components/PostFeed.jsx
+++ ui-service/src/components/PostFeed.jsx
@@ -30,6 +30,7 @@ export const PostFeed = (props) => {
     const [comments, setComments] = useState([]);
     const [likes, setLikes] = useState([]); 
     const [commentInputField, setCommentInputField] = useState({});
+    const [commentMarkdownField, setCommentMarkdownField] = useState({});
     const [editBodyField, setEditBodyField] = useState({});
     const [editTitleField, setEditTitleField] = useState({});
     const [editPost, setEditPost] = useState({});
@@ -106,6 +107,15 @@ export const PostFeed = (props) => {
         })
         console.log(commentInputField)
     }
+
+    const commentMarkdownHandler = (postID, value) => {
+        //console.log(postID, value)
+        setCommentMarkdownField({
+            ...commentMarkdownField,
+            [postID]: value,
+        })
+        console.log(commentMarkdownField)
+    } 
 
     const editTitleHandler = (postID, edit) => {
         console.log(postID, edit)
@@ -217,6 +227,11 @@ export const PostFeed = (props) => {
         let newComment = commentInputField[postID];
         let currentDate = new Date();
         let url = `${postID}/comments`
+        let useMarkdown = commentMarkdownField[postID];
+        let contentType = "text/plain"
+        if(useMarkdown === true){
+            contentType = "text/markdown"
+        }
         currentDate = currentDate.toString();
         console.log(newComment)
         try{
@@ -226,7 +241,7 @@ export const PostFeed = (props) => {
                     publishedTime: currentDate, //TODO: ADD MORE FIELDS
                     authorID: authorInfo.id,
                     content: commentInputField[postID],
-                    contentType: "text/plain", //TODO: ALLOW TEXT/MARKDOWN
+                    contentType: contentType, //TODO: ALLOW TEXT/MARKDOWN
                 }
                 // id: "1234",
                 //published: currentDate
@@ -289,7 +304,7 @@ export const PostFeed = (props) => {
                 {/* React Section */}
                 <ReactionSection post={post} likes={likes[i]} clickHandler={reactionClickHandler} shareHandler={sharePostHandler}></ReactionSection>
                 {/* Comment Section */}
-                <CommentSection post={post} comments={comments[i]} submitHandler={submitCommentHandler} changeHandler={commentChangeHandler}></CommentSection>
+                <CommentSection post={post} comments={comments[i]} submitHandler={submitCommentHandler} changeHandler={commentChangeHandler} checkHandler={commentMarkdownHandler}></CommentSection>
             </div>
             )}
         </div>
